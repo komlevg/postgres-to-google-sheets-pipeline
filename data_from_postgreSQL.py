40f2d3a3fc2400b6c5827d30c360195f6052cc32
komlevg <komlev3@gmail.com>
2026-06-29 14:55:48 +0300

    new version ETL

--- data_from_postgreSQL.py
+++ data_from_postgreSQL.py
@@ -21,7 +21,7 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
-GOOGLE_SHEET_NAME = os.getenv("GOOGLE_SHEET_NAME")
+GOOGLE_SHEET_ID = os.getenv("GOOGLE_SHEET_ID")
 
 
 # =====================================================
@@ -42,13 +42,8 @@ engine = create_engine(DATABASE_URL)
 # =====================================================
 
 query = """
-SELECT
-    customer_id,
-    customer_name,
-    total_orders,
-    last_order_date
-FROM customers
-ORDER BY customer_id
+SELECT *
+FROM trades;
 """
 
 
@@ -71,20 +66,20 @@ print(f"Получено строк: {len(df)}")
 
 print("Преобразование данных...")
 
-df["last_order_date"] = pd.to_datetime(
-    df["last_order_date"]
-)
-
-df["days_since_order"] = (
-    pd.Timestamp.now()
-    - df["last_order_date"]
-).dt.days
-
-df["customer_name"] = (
-    df["customer_name"]
-    .str.strip()
-    .str.title()
-)
+# df["last_order_date"] = pd.to_datetime(
+#     df["last_order_date"]
+# )
+#
+# df["days_since_order"] = (
+#     pd.Timestamp.now()
+#     - df["last_order_date"]
+# ).dt.days
+#
+# df["customer_name"] = (
+#     df["customer_name"]
+#     .str.strip()
+#     .str.title()
+# )
 
 print("Преобразование завершено")
 
@@ -104,12 +99,15 @@ credentials = Credentials.from_service_account_file(
     "service_account.json",
     scopes=SCOPES
 )
+print("Sheet ID:", GOOGLE_SHEET_ID)
+print("Service account:", credentials.service_account_email)
 
 client = gspread.authorize(credentials)
 
-spreadsheet = client.open(
-    GOOGLE_SHEET_NAME
-)
+for sheet in client.openall():
+    print(sheet.title)
+
+spreadsheet = client.open_by_key(GOOGLE_SHEET_ID)
 
 worksheet = spreadsheet.worksheet(
     "Import"
@@ -124,6 +122,10 @@ worksheet = spreadsheet.worksheet(
 print("Обновление Google Sheets...")
 
 worksheet.clear()
+
+# Преобразовать все datetime-колонки в строки
+for col in df.select_dtypes(include=["datetime64[ns]", "datetimetz"]).columns:
+    df[col] = df[col].dt.strftime("%Y-%m-%d %H:%M:%S")
 
 data = [df.columns.tolist()] + df.values.tolist()
 
